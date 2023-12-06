f6a2652467f9f6397c52a24f056be8561e7b7e04
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-12-06 21:33:25 +0100

    first commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from 'vue';
+    import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div id="MainLayout" class="w-full h-screen">
+        <div>
+            Main Layout
+        </div>
+
+        <slot />
+        
+    </div>
+</template>
