84732a844faf155084939483984f0d7fd4b1a548
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2023-12-07 19:26:12 +0100

    Template

---
+++ resources/js/Components/FullScreen.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <li class="nav-item">
+        <a class="nav-link" 
+           data-widget="fullscreen" 
+           href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+    </li>
+</template>
---
+++ resources/js/Components/LeftSidebar.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <li class="nav-item">
+        <a class="nav-link" 
+           data-widget="control-sidebar" 
+           data-slide="true" 
+           href="#" role="button">
+            <i class="fas fa-th-large"></i>
+        </a>
+    </li>
+</template>
